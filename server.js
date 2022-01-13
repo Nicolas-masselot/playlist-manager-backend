fb88cbcf03067e6d140683e907b8789a198b7520
nicolas-masselot <nicolasmasselot0@gmail.com>
2022-01-13 15:48:21 +0100

    correction du problème de blocage CORS

--- server.js
+++ server.js
@@ -15,12 +15,21 @@ app.use(bodyParser.json({ limit: "5mb" }));
 // app.get("/", (req, res) => res.send("Hello World"));
 const cors = require("cors");
 
-const options = {
-    origin: ["https://playlist-manager-admin.herokuapp.com/", "https://playlist-manager-user.herokuapp.com/"],
-};
-
-app.use(cors(options));
-app.options('*', cors())
+let whitelist = ["https://playlist-manager-admin.herokuapp.com/", "https://playlist-manager-user.herokuapp.com/"];
+app.use(
+    cors({
+        origin: function(origin, callback) {
+            // allow requests with no origin
+            if (!origin) return callback(null, true);
+            if (whitelist.indexOf(origin) === -1) {
+                var message = "The CORS policy for this origin doesn't allow access from the particular origin.";
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        },
+        credentials: true,
+    })
+);
 
 app.use("/", routes);
 app.use(express.static("./adverts"));
